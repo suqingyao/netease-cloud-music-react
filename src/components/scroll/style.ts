e6c32b75d39fac5e7642593ea0e1eb5642868ee0
cullyfung <fcx.cli@qq.com>
2022-09-08 23:00:22 +0800

    fix: scroll

--- src/components/scroll/style.ts
+++ src/components/scroll/style.ts
@@ -1,14 +1,8 @@
 import styled from 'styled-components'
 
 export const ScrollWrapper = styled.div`
-  display: ${(props: any) =>
-    props.direction === 'horizontal' ? 'inline-block' : ''};
-  width: ${(props: any) => props.wrapWidth};
-  width: ${(props: any) => props.wrapHeight};
+  display: block;
   overflow: hidden;
 `
 
-export const ScrollContent = styled.div`
-  display: ${(props: any) =>
-    props.direction === 'horizontal' ? 'inline-block' : ''};
-`
+export const ScrollContent = styled.div``
