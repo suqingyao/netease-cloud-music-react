11c7a16629d2ce44c79432cea1d15779f3ced93d
cullyfung <fcx.cli@qq.com>
2022-09-15 23:29:37 +0800

    chore: update prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none'
+}
